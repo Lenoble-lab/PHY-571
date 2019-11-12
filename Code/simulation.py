4dc9fc70149e9a52fd31edb97fe1de4dcca3a254
Lenoble-lab <55918962+Lenoble-lab@users.noreply.github.com>
2019-11-13 00:31:37 +0100

    Update simulation.py

--- Code/simulation.py
+++ Code/simulation.py
@@ -8,19 +8,19 @@ class Simulation :
         self.center = center        #center of the grid
         self.G = G                  #gravitation constante
         self.particules = []        #particules, needs to be update
-        self.root                   #root of the tree
+        self.root = Node(Particule(self.center, 0, 0, 0, -1), 0, 0)            #root of the tree
         self.theta = theta          #parameter of the simulation
     
     def step(self):
         self.set_tree()                 #create the tree with the current state of the particules
+        print("set_tree finish")
         self.calculate_force()          #calcul the interaction on each particule
-        self.calculate_acceleration()   #calculate acceleration
         self.update_velocity()          #update the velocity of each particule
         self.update_position()          #update the position of each particule
 
 
     def set_tree(self):
-        root_particule = Particule(self.center, 0, 0, 0, -1)
+        root_particule = Particule(self.center, 0, np.array([0,0]), np.array([0,0]), -1)
         root_node = Node(root_particule, self.size, self.center)
 
         tree = Tree(root_node)
@@ -29,35 +29,39 @@ class Simulation :
 
     def calculate_force(self):
         for i in range (self.nb_point):
-            self.particules[i].force = 0 # re-set the force for the next step
-            self.calculate_force_target(self.particules[i])
+            self.particules[i].force = np.array([0.,0.]) # re-set the force for the next step
+            self.calculate_force_target(self.particules[i], self.root)
     
-    def calculate_force_target(self, target_particule, temp_node = self.root):
-        
-        particule2 = temp_node.particule  #particule (or virtual particule) in question
+    def calculate_force_target(self, target_particule, temp_node):
+        particule2 = temp_node.virtual_particule  #particule (or virtual particule) in question
 
         vect_r = particule2.position - target_particule.position
         r = np.sqrt(np.sum(vect_r**2))
 
         d = temp_node.box_size
+        print(r, ' r')
 
-        if (d/r < self.theta) or (temp_node.nb_children == 0):  
+        if r>0:  #if the node has only one particule
             
-            #if the node is far enough then we approximate
-            #or termination condition
+            print(d/r, " d/r")
+            print(target_particule.force, 'target part force')
 
-            target_particule.force += self.G * particule2.mass * target_particule.mass / r**2
-        
-        else :  
-            #not favorable case
+            if (temp_node.nb_children == 0) or (d/r <=  self.theta): 
+                
+                #if the node is far enough then we approximate
+                #or termination condition
 
-            for i in range(4):
-                if temp_node.children[i] != 0 :
-                    target_particule.force += calculate_force_target(target_particule, temp_node.children[i])
+                target_particule.force += self.G * particule2.mass * target_particule.mass / r**3 * vect_r
+            
+            else :  
+                #not favorable case, split the node and repeat
+
+                for i in range(4):
+                    if temp_node.children[i] != None :
+                        target_particule.force += self.calculate_force_target(target_particule, temp_node.children[i])
     
 
-
-    def calculate_velocity (self) :
+    def update_velocity (self) :
         for i in range(self.nb_point):
             self.particules[i].velocity += (-self.particules[i].force/self.particules[i].mass )*self.delta_t   #1st order of integration
 
